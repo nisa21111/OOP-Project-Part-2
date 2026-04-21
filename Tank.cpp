a5bc3e5cac8ef6ff50b4f59e217b6f7ec4360309
nisa21111 <d3nyb3l@gmail.com>
2026-04-21 11:11:20 +0300

    Added Tank class

---
+++ Tank.cpp
@@ -0,0 +1,66 @@
+#include "Tank.h"
+
+Tank::Tank() : Robot("Tank", 600, 10, 15, 200){}
+
+Tank::Tank(std::string name) : Robot(name, 600, 10, 15, 200){}
+
+Tank::Tank(const Tank& other) : Robot(other){}
+
+Tank& Tank::operator=(const Tank& other){
+    if (this != &other){
+            Robot::operator=(other);
+    }
+    return *this;
+}
+
+Tank::~Tank(){}
+
+std::ostream& operator<<(std::ostream os, const Tank& t){
+    os << (const Robot&)t;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Tank& t){
+    is >> (Robot&)t;
+    return is;
+}
+
+void Tank::saveToFile(std::ostream& out) const {
+    out<< "Tank\n";
+    Robot::saveToFile(out);
+}
+
+void Tank::loadFromFile(std::istream& in){
+    Robot::loadFromFile(in);
+}
+
+void Tank::attack(Robot& enemy){
+    if (energy < 10){
+        std::cout << "Not enough energy!\n";
+        return;
+    }
+
+    energy -= 10;
+    enemy.takeDamage(damage);
+    std::cout << name << " attacks for " << damage << " damage!\n";
+}
+
+void Tank::useSpecialAbility(){
+    if (energy < 40){
+        std::cout << "Not enough energy!\n";
+        return;
+    }
+
+    energy -= 40;
+    isSpecialActive = true;
+
+    std::cout << name << " activated shield! (takes reduced damage next hit)\n";
+}
+
+void Tank::takeDamage(int amount){
+    if (isSpecialActive = true){
+        amount /= 2;
+        isSpecialActive = false;
+    }
+    Robot::takeDamage(amount);
+}
---
+++ Tank.h
@@ -0,0 +1,25 @@
+#ifndef TANK_H
+#define TANK_H
+
+#include "Robot.h"
+
+class Tank : public Robot{
+public:
+    Tank();
+    Tank(std::string name);
+    Tank(const Tank& other);
+    Tank& operator=(const Tank& other);
+    ~Tank();
+
+    friend std::ostream& operator<<(std::ostream& os, const Tank& t);
+    friend std::istream& operator>>(std::istream& is, Tank& t);
+
+    void saveToFile(std::ostream& out) const override;
+    void loadFromFile (std::istream& in) override;
+
+    void attack(Robot& enemy) override;
+    void useSpecialAbility () override;
+    void takeDamage(int amount) override;
+};
+
+#endif
